f83ae5698aba9e5717e4fef6a4c94fd9e06ec1db
Hector Rivera <i410hlr@gmail.com>
2019-12-20 08:44:13 -0500

    Delete budgetAPI.js

--- routes/budgetAPI.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-//ROUTE -- Default Default GET
-router.get('/',(req,res)=>{
-  res.send('Course Definition');
-});
-
-module.exports=router;
